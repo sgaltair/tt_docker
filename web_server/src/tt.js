d5d24f2fcefb700e1f1d9fb94580fc050ebda218
Stephen Griffith <sgriffith0891@gmail.com>
2022-11-17 21:31:27 -0500

    Linked to MongoDB Atlas database.

--- web_server/src/tt.js
+++ web_server/src/tt.js
@@ -8,7 +8,7 @@ let accuracy_text = document.querySelector(".curr_accuracy");
 let error_text = document.querySelector(".curr_errors");
 let cpm_text = document.querySelector(".curr_cpm");
 let wpm_text = document.querySelector(".curr_wpm");
-let awpm_text = document.querySelector(".curr_awpm")
+let awpm_text = document.querySelector(".curr_awpm");
 let quote_text = document.querySelector(".quote");
 let input_area = document.querySelector(".input_area");
 let restart_btn = document.querySelector(".restart_btn");
@@ -28,188 +28,180 @@ let current_quote = "";
 let quoteNo = 0;
 let timer = null;
 
-
 function updateQuote() {
-  quote_text.textContent = null;
-  current_quote = quotes_array[quoteNo];
-  
-  // separate each character and make an element 
-  // out of each of them to individually style them
-  current_quote.split('').forEach(char => {
-    const charSpan = document.createElement('span')
-    charSpan.innerText = char
-    quote_text.appendChild(charSpan)
-  })
-  
-  // roll over to the first quote
-  if (quoteNo < quotes_array.length - 1)
-  quoteNo++;
-  else
-  quoteNo = 0;
+	quote_text.textContent = null;
+	current_quote = quotes_array[quoteNo];
+
+	// separate each character and make an element
+	// out of each of them to individually style them
+	current_quote.split("").forEach((char) => {
+		const charSpan = document.createElement("span");
+		charSpan.innerText = char;
+		quote_text.appendChild(charSpan);
+	});
+
+	// roll over to the first quote
+	if (quoteNo < quotes_array.length - 1) quoteNo++;
+	else quoteNo = 0;
 }
 
 function processCurrentText() {
-  
-  // get current input text and split it
-  curr_input = input_area.value;
-  curr_input_array = curr_input.split('');
-  
-  // increment total characters typed
-  characterTyped++;
-  
-  errors = 0;
-  
-  quoteSpanArray = quote_text.querySelectorAll('span');
-  quoteSpanArray.forEach((char, index) => {
-    let typedChar = curr_input_array[index]
-    
-    // characters not currently typed
-    if (typedChar == null) {
-      char.classList.remove('correct_char');
-      char.classList.remove('incorrect_char');
-      
-      // correct characters
-    } else if (typedChar === char.innerText) {
-      char.classList.add('correct_char');
-      char.classList.remove('incorrect_char');
-      
-      // incorrect characters
-    } else {
-      char.classList.add('incorrect_char');
-      char.classList.remove('correct_char');
-      
-      // increment number of errors
-      errors++;
-    }
-  });
-  
-  // display the number of errors
-  error_text.textContent = total_errors + errors;
-  
-  // update accuracy text
-  let correctCharacters = (characterTyped - (total_errors + errors));
-  accuracyVal = ((correctCharacters / characterTyped) * 100);
-  accuracy_text.textContent = Math.round(accuracyVal);
-  
-  // if current text is completely typed
-  // irrespective of errors
-  if (curr_input.length == current_quote.length) {
-    updateQuote();
-    
-    // update total errors
-    total_errors += errors;
-    
-    // clear the input area
-    input_area.value = "";
-  }
+	// get current input text and split it
+	curr_input = input_area.value;
+	curr_input_array = curr_input.split("");
+
+	// increment total characters typed
+	characterTyped++;
+
+	errors = 0;
+
+	quoteSpanArray = quote_text.querySelectorAll("span");
+	quoteSpanArray.forEach((char, index) => {
+		let typedChar = curr_input_array[index];
+
+		// characters not currently typed
+		if (typedChar == null) {
+			char.classList.remove("correct_char");
+			char.classList.remove("incorrect_char");
+
+			// correct characters
+		} else if (typedChar === char.innerText) {
+			char.classList.add("correct_char");
+			char.classList.remove("incorrect_char");
+
+			// incorrect characters
+		} else {
+			char.classList.add("incorrect_char");
+			char.classList.remove("correct_char");
+
+			// increment number of errors
+			errors++;
+		}
+	});
+
+	// display the number of errors
+	error_text.textContent = total_errors + errors;
+
+	// update accuracy text
+	let correctCharacters = characterTyped - (total_errors + errors);
+	accuracyVal = (correctCharacters / characterTyped) * 100;
+	accuracy_text.textContent = Math.round(accuracyVal);
+
+	// if current text is completely typed
+	// irrespective of errors
+	if (curr_input.length == current_quote.length) {
+		updateQuote();
+
+		// update total errors
+		total_errors += errors;
+
+		// clear the input area
+		input_area.value = "";
+	}
 }
 
 function updateTimer() {
-  if (timeLeft > 0) {
-    // decrease the current time left
-    timeLeft--;
-    
-    // increase the time elapsed
-    timeElapsed++;
-    
-    // update the timer text
-    timer_text.textContent = timeLeft + "s";
-  }
-  else {
-    // finish the game
-    finishGame();
-  }
+	if (timeLeft > 0) {
+		// decrease the current time left
+		timeLeft--;
+
+		// increase the time elapsed
+		timeElapsed++;
+
+		// update the timer text
+		timer_text.textContent = timeLeft + "s";
+	} else {
+		// finish the game
+		finishGame();
+	}
 }
-function httpGet(theUrl){
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open ("get", theUrl, false);
-  xmlHttp.send( null );
-  return xmlHttp.responseText;
+function httpGet(theUrl) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("get", theUrl, false);
+	xmlHttp.send(null);
+	return xmlHttp.responseText;
 }
 function finishGame() {
-  // stop the timer
-  clearInterval(timer);
-  
-  // disable the input area
-  input_area.disabled = true;
-  
-  // show finishing text
-  quote_text.textContent = "Click on restart to start the test again. All attempts are recorded.";
-  
-  // display restart button
-  restart_btn.style.display = "block";
-  
-  // calculate cpm and wpm and awpm
-  cpm = Math.round(((characterTyped / timeElapsed) * 60));
-  wpm = Math.round((((characterTyped / 5) / timeElapsed) * 60));
-  awpm = Math.round(wpm * (accuracyVal / 100))
-  
-  // update cpm and wpm and awpm text
-  cpm_text.textContent = cpm;
-  wpm_text.textContent = wpm;
-  awpm_text.textContent = awpm;
-  
-  // display the cpm and wpm and awpm
-  cpm_group.style.display = "block";
-  wpm_group.style.display = "block";
-  awpm_group.style.display = "block";
-  
-  let USER = Math.floor(Math.random() * 100);
-  let timestamp = new Date();
-  let publicIp = httpGet("https://api.ipify.org/");
-  const body =`  "${USER}":{
-    "Timestamp": "${timestamp}",
-    "IP": "${publicIp}",
-    "WPM": ${wpm},
-    "AWPM": ${awpm},
-    "AccuracyPercent": ${accuracyVal}
-  },`
-  
-  const init = {
-    method: 'POST',
-    body
-    };
-    
-    fetch('http://isame-lab.com:8075', init)
-    .then((response) => {
-      return response.json(); // or .text() or .blob() ...
-    })
-    .then((text) => {
-      // text is the response body
-    })
-    .catch((e) => {
-      // error in e.message
-    });
+	// stop the timer
+	clearInterval(timer);
+
+	// disable the input area
+	input_area.disabled = true;
+
+	// show finishing text
+	quote_text.textContent =
+		"Click on restart to start the test again. All attempts are recorded.";
+
+	// display restart button
+	restart_btn.style.display = "block";
+
+	// calculate cpm and wpm and awpm
+	cpm = Math.round((characterTyped / timeElapsed) * 60);
+	wpm = Math.round((characterTyped / 5 / timeElapsed) * 60);
+	awpm = Math.round(wpm * (accuracyVal / 100));
+
+	// update cpm and wpm and awpm text
+	cpm_text.textContent = cpm;
+	wpm_text.textContent = wpm;
+	awpm_text.textContent = awpm;
+
+	// display the cpm and wpm and awpm
+	cpm_group.style.display = "block";
+	wpm_group.style.display = "block";
+	awpm_group.style.display = "block";
+
+	let USER = Math.floor(Math.random() * 100);
+	let timestamp = new Date();
+	let publicIp = httpGet("https://api.ipify.org/");
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+	var urlencoded = new URLSearchParams();
+	urlencoded.append("timestamp", timestamp);
+	urlencoded.append("ip", publicIp);
+	urlencoded.append("wpm", wpm);
+	urlencoded.append("awpm", awpm);
+	urlencoded.append("accuracypercent", accuracyVal);
+
+	var requestOptions = {
+		method: "POST",
+		headers: myHeaders,
+		body: urlencoded,
+		redirect: "follow",
+	};
+
+	fetch("http://isame-lab.com:8011/users", requestOptions)
+		.then((response) => response.text())
+		.then((result) => console.log(result))
+		.catch((error) => console.log("error", error));
 }
 
-
 function startGame() {
+	resetValues();
+	updateQuote();
 
-  resetValues();
-  updateQuote();
-
-  // clear old and start a new timer
-  clearInterval(timer);
-  timer = setInterval(updateTimer, 1000);
+	// clear old and start a new timer
+	clearInterval(timer);
+	timer = setInterval(updateTimer, 1000);
 }
 
 function resetValues() {
-  timeLeft = TIME_LIMIT;
-  timeElapsed = 0;
-  errors = 0;
-  total_errors = 0;
-  accuracy = 0;
-  characterTyped = 0;
-  quoteNo = 0;
-  input_area.disabled = false;
+	timeLeft = TIME_LIMIT;
+	timeElapsed = 0;
+	errors = 0;
+	total_errors = 0;
+	accuracy = 0;
+	characterTyped = 0;
+	quoteNo = 0;
+	input_area.disabled = false;
 
-  input_area.value = "";
-  quote_text.textContent = 'Click on the area below to start the game.';
-  accuracy_text.textContent = 100;
-  timer_text.textContent = timeLeft + 's';
-  error_text.textContent = 0;
-  restart_btn.style.display = "none";
-  cpm_group.style.display = "none";
-  wpm_group.style.display = "none";
-  awpm_group.style.display = "none";
+	input_area.value = "";
+	quote_text.textContent = "Click on the area below to start the game.";
+	accuracy_text.textContent = 100;
+	timer_text.textContent = timeLeft + "s";
+	error_text.textContent = 0;
+	restart_btn.style.display = "none";
+	cpm_group.style.display = "none";
+	wpm_group.style.display = "none";
+	awpm_group.style.display = "none";
 }
